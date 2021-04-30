e9b22546c5396f0e5d75d42070d197e8b30f63c3
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2021-04-30 13:55:06 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+PUPPETEER_PRODUCT=chrome deno run -A --unstable https://deno.land/x/puppeteer@9.0.0/install.ts
+
+deno install -n underleaf --unstable --allow-env --allow-read --allow-write https://github.com/JonEsparaz/underleaf/releases/download/v1.0.0-beta/underleaf.ts
